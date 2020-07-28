7a7904738ae64da8aabe56c1e31634af3b0b7db9
jaydipgabani <gabanijaydip@gmail.com>
2020-07-28 13:20:00 -0400

    initial push

---
+++ controllers/apps/deployment_controller.go
@@ -0,0 +1,105 @@
+/*
+
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+	dm "datamover/apis/apps/v1"
+	ct "datamover/apis/stable.example.com/v1"
+	"flag"
+	"fmt"
+	"github.com/go-logr/logr"
+	"github.com/kubernetes-csi/external-snapshotter/v2/pkg/apis/volumesnapshot/v1beta1"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+)
+
+// DeploymentReconciler reconciles a Deployment object
+type DeploymentReconciler struct {
+	client.Client
+	Log    logr.Logger
+	Scheme *runtime.Scheme
+}
+
+// Annotation for generating RBAC role to Watch Pods
+// +kubebuilder:rbac:groups="",resources=pods,verbs=get;watch;list
+// +kubebuilder:rbac:groups=apps.k8s.io,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=apps.k8s.io,resources=deployments/status,verbs=get;update;patch
+
+func (r *DeploymentReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
+	_ = context.Background()
+	log := r.Log.WithValues("deployment", req.NamespacedName)
+	name := "gp2"
+	// your logic here
+	log.V(1).Info(fmt.Sprintf("%#v", r))
+	log.V(1).Info(fmt.Sprintf("%#v", req))
+	pvc := &v1.PersistentVolumeClaim{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "snp-pvc",
+			Namespace: "nginx-example",
+		},
+		Spec: v1.PersistentVolumeClaimSpec{
+			StorageClassName: &name,
+			AccessModes:      []v1.PersistentVolumeAccessMode{v1.ReadWriteOnce},
+			Resources: v1.ResourceRequirements{
+				Requests: v1.ResourceList{
+					v1.ResourceName(v1.ResourceStorage): resource.MustParse("2Gi"),
+				},
+			},
+		},
+	}
+
+	tab := &ct.CronTab{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "snp-pvc",
+			Namespace: "nginx-example",
+		},
+		Spec: ct.CronTabSpec{
+			Foo: "example",
+		},
+	}
+	kubeconfig := "/home/jgabani/Downloads/jgabani0518/ocp4-workshop_jgabani0518_kubeconfig"
+	flag.Parse()
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		print(tab)
+		panic(err.Error())
+	}
+	// creates the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+	clientset.CoreV1().PersistentVolumeClaims("nginx-example").Create(pvc)
+
+	return ctrl.Result{}, nil
+}
+
+func (r *DeploymentReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&dm.Deployment{}).
+		Watches(&source.Kind{Type: &v1beta1.VolumeSnapshot{}}, &handler.EnqueueRequestForObject{}).
+		Complete(r)
+}
